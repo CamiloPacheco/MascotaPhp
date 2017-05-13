b7684a3d2020427f4642bd660738398bc0f2ffd0
USER <USER@PC>
2017-05-13 17:11:24 -0500

    Inicio aplicacion web

--- index.php
+++ index.php
@@ -10,19 +10,37 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-    
+      
 <H1 align="center"> Encuentra tu mascota </H1>.
 <H2 align = " left "> Iniciar sesión</H2>
 
 <body style="background:#80BFFF">
-  <form name="formularioDatos" method="post" >
+  <form name="formularioDatos" method="POST" >
 
   Usuario: <input type="text" name="usuario" value="">
   <br/> <br/>
   Contraseña: <input type="password" name="contraseña" value="">
   <br/> <br/>
   <input value="Iniciar seccion" type="submit" />
+ <?php
+        
+        
+ $servidor="localhost";
+ $usuario="root";
+ $clave="contraseña";
+ $base="mascotadatabase";
+/* @var $link type */
+$link = mysqli_connect($servidor, $usuario, $clave, $base);
+mysqli_select_db($link, $base);
 
+   
+        
+        $user=filter_input(INPUT_POST, 'Usuario');
+        $pass=filter_input(INPUT_POST, 'Contraseña');
+
+
+mysqli_query("INSERT INTO persona(Usuario,Contraseña)values('$user','$pass')");
+        ?>
 </form>
 
     </body>
