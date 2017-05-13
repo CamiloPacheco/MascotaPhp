fe99765ba857b9933b69304642914a22ccf1ce90
USER <USER@PC>
2017-05-13 18:22:42 -0500

    Inicio aplicacion web

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ and open the template in the editor.
         
  $servidor="mascotaserver.database.windows.net";
  $usuario="administrador";
- $clave="contraseña";
+ $clave="Contraseña01";
  $base="MascotaDataBase";
 
 $link = mysqli_connect($servidor, $usuario, $clave, $base);
